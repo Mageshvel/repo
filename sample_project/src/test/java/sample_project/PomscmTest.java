f5742b0845f03b1aec5422d34c7624d095c69d04
ADMIN <ADMIN@192.168.137.102>
2025-05-21 16:08:45 +0530

    qwerty

--- sample_project/src/test/java/sample_project/PomscmTest.java
+++ sample_project/src/test/java/sample_project/PomscmTest.java
@@ -3,6 +3,7 @@ package sample_project;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
+import org.testng.annotations.Test;
 
 public class PomscmTest {
 	@Test
